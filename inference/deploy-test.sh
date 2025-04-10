ebc3347867ba4d5afbf88d8ec6d22bc74d5dc82d
Eugene Fedorenko <eugene.fedor@gmail.com>
2025-04-09 17:53:34 -0700

    updates

--- inference/deploy-test.sh
+++ inference/deploy-test.sh
@@ -11,5 +11,5 @@ curl -v -H "Host: ${SERVICE_HOSTNAME}" -H "Content-Type: application/json" -d '{
 docker build -t tacos-inference ./inference 
 docker run -it -p 8000:8000 tacos-inference 
 
-curl -H "Content-Type: application/json" -d '{"image":"https://www.inspiredtaste.net/wp-content/uploads/2018/03/Easy-Ground-Pork-Tacos-Recipe-3-1200.jpg"}' http://localhost:8000/predict
+curl -H "Content-Type: application/json" -d '{"image":"https://lp-cms-production.imgix.net/image_browser/tacos_mexico_G.jpg?auto=format&fit=crop&sharp=10&vib=20&ixlib=react-8.6.4&w=850&q=20&dpr=5"}' http://localhost:8000/predict
 
